a5e9cef65ddc0675a5ee21b681f68f7327d2ad3c
jayasurya-sparkout <jayasurya@sparkouttech.com>
2026-02-28 11:59:56 +0530

    feat : UI uodates

--- src/components/common/Input.jsx
+++ src/components/common/Input.jsx
@@ -1,11 +1,28 @@
 
+import { Search } from "lucide-react"
 
-export const Input = () => {
+export const Input = (
+    {
+        type,
+        id,
+        name,
+        className,
+        placeholder
+    }
+) => {
 
     return (
-        <>
-            Input
-        </>
+        <div className="searchInputWrapper">
+            <Search size={20} className="searchIcon" />
+            <input
+                className={`${className ? className : ""} `}
+                type={type}
+                id={id}
+                name={name}
+                placeholder={placeholder ? placeholder : ""}
+                autoComplete="off"
+            />
+        </div>
     )
 
 }
